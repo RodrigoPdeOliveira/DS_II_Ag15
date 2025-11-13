62764a9c21fdf3f944776a1c1a1d1d9f2cc9db6a
Rodrigo Oliveira <rodrikpoliveira@gmail.com>
2025-11-13 10:41:24 -0300

    Página de resumo adicionada

---
+++ Views/ResumoEventoPage.xaml.cs
@@ -0,0 +1,18 @@
+using CadastroEventosApp.Models;
+
+namespace CadastroEventosApp.Views;
+
+public partial class ResumoEventoPage : ContentPage
+{
+	public ResumoEventoPage(Evento evento)
+	{
+		InitializeComponent();
+
+		BindingContext = evento;
+	}
+
+	private async void OnVoltarClicked(object sender, EventArgs e)
+	{
+		await Navigation.PopAsync();
+	}
+}
